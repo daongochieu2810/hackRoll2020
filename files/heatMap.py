a78dc71fb61f5cde0f26cbc18362c74c6868995e
Adithya Narayan <r.s.adithya.narayan@gmail.com>
2020-01-19 14:33:06 +0800

    Update heatMap.py

--- files/heatMap.py
+++ files/heatMap.py
@@ -30,23 +30,6 @@ with open("./data.csv") as f:
                 data[1].extend(longitudes[j*25:(j+1)*25])
         gmap = gmplot.GoogleMapPlotter(1.3608985, 103.8052229, 11)
         gmap.heatmap(data[0], data[1])
-        gmap.apikey = "AIzaSyArTD5eoWye3V6UY2k33dO5cdNq92cz5aQ"
         gmap.draw(
             "./heatMapFiles/heatMap"+str(i)+".html")
 
-
-# for i in range(1, 100):
-#     raw_data = pd.read_csv(
-#         "/home/adi/Desktop/Code/Python/EliteVision/cctvLocs.csv")
-#     data = raw_data.head(120-i)
-
-#     latitudes = data["LAT"]
-#     longitudes = data["LONG"]
-
-#     gmap = gmplot.GoogleMapPlotter(1.3608985, 103.8052229, 11)
-
-#     gmap.heatmap(latitudes, longitudes)
-
-#     gmap.apikey = "AIzaSyArTD5eoWye3V6UY2k33dO5cdNq92cz5aQ"
-#     gmap.draw(
-#         "/home/adi/Desktop/Code/Python/EliteVision/heatMapFiles/heatMap"+str(i)+".html")
